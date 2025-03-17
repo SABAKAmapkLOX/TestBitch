0dc60801b452c8a03cf9789586bd0bfa3736ba38
ИСП-34 <-34@LAB24-09>
2025-03-17 10:43:07 +0300

    Добавьте файлы проекта.

---
+++ BigDickTest/Test1.cs
@@ -0,0 +1,42 @@
+﻿using TestBitch;
+
+namespace BigDickTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+
+        [TestMethod]
+        public void PolosaException()
+        {
+            double initialSpeed = 5;
+            double finalSpeed = 20;
+            double acceleration = 50;
+
+            Assert.AreEqual(187.5, Test.Bolid(100,50,20), "Ошибка");
+        }
+
+        [TestMethod]
+        public void PolosaException2()
+        {
+            double initialSpeed = 5;
+            double finalSpeed = 20;
+            double acceleration = 50;
+
+            Assert.AreEqual(3.7, Test.Bolid(finalSpeed, initialSpeed, acceleration), "Ошибка");
+        }
+
+        [TestMethod]
+        public void PolosaException3()
+        {
+            Assert.AreEqual(187.5, Test.Bolid(100, 50, 20), "Ошибка");
+        }
+
+        [TestMethod]
+        public void PolosaException4()
+        {
+            double result = Test.Bolid(200, 100, 100);
+            Assert.AreEqual(150, result, "Ошибка");
+        }
+    }
+}
---
+++ TestBitch/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace TestBitch
+{
+    public class Test
+    {
+        public static double Bolid(double finalSpeed, double initialSpeed, double acceleration)
+        {
+            return (Math.Pow(finalSpeed, 2) - Math.Pow(initialSpeed, 2)) / (2 * acceleration);
+        }
+    }
+}
